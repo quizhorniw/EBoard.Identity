894e2b575d17311e6d5711d94bbc93e6d33b3ef7
Ревотюк Даниил <danistollen@yahoo.com>
2025-09-05 18:45:28 +0300

    Import static members;fix test names

--- SolarLab.EBoard.Identity.UnitTests/Domain/Entities/UserTests.cs
+++ SolarLab.EBoard.Identity.UnitTests/Domain/Entities/UserTests.cs
@@ -1,4 +1,5 @@
 using SolarLab.EBoard.Identity.Domain.Entities;
+using static Xunit.Assert;
 
 namespace SolarLab.EBoard.Identity.UnitTests.Domain.Entities;
 
@@ -18,21 +19,21 @@ public class UserTests
         var user = new User(email, phoneNumber, firstName, lastName, passwordHash);
         
         // Assert
-        Assert.NotEqual(Guid.Empty, user.Id);
-        Assert.Equal(email, user.Email);
-        Assert.Equal("User", user.Role);
+        NotEqual(Guid.Empty, user.Id);
+        Equal(email, user.Email);
+        Equal("User", user.Role);
     }
 
     [Fact]
-    public void WhenUserIsCreated_RaisesUserRegisteredDomainEvent()
+    public void WhenUserCreated_RaisesUserRegisteredDomainEvent()
     {
         // Act
         var user = new User("test@mail.com", "+79180576819", "Иван", "Иванов", "hash");
 
         // Assert
-        Assert.Single(user.DomainEvents);
-        var @event = Assert.IsType<UserRegisteredDomainEvent>(user.DomainEvents.First());
-        Assert.Equal(user.Id, @event.UserId);    
+        Single(user.DomainEvents);
+        var @event = IsType<UserRegisteredDomainEvent>(user.DomainEvents.First());
+        Equal(user.Id, @event.UserId);    
     }
 
     [Theory]
@@ -44,7 +45,7 @@ public class UserTests
     {
         // Act
         // Assert
-        Assert.Throws<ArgumentException>(() => 
+        Throws<ArgumentException>(() => 
             new User(email, "+79180576819", "Иван", "Иванов", "hash"));
     }
 
@@ -53,11 +54,11 @@ public class UserTests
     [InlineData("some-letters")]
     [InlineData("12345")]
     [InlineData("+12345")]
-    public void When_CreateUser_WithInvalidPhoneNumber_ThrowsArgumentException(string phoneNumber)
+    public void WhenCreateUser_WithInvalidPhoneNumber_ThrowsArgumentException(string phoneNumber)
     {
         // Act
         // Assert
-        Assert.Throws<ArgumentException>(() => 
+        Throws<ArgumentException>(() => 
             new User("test@mail.com", phoneNumber, "Иван", "Иванов", "hash"));
     }
 
@@ -65,11 +66,11 @@ public class UserTests
     [InlineData(null)]
     [InlineData("")]
     [InlineData("text with whitespaces\r\n")]
-    public void When_CreateUser_WithInvalidFirstName_ThrowsArgumentException(string firstName)
+    public void WhenCreateUser_WithInvalidFirstName_ThrowsArgumentException(string firstName)
     {
         // Act
         // Assert
-        Assert.Throws<ArgumentException>(() => 
+        Throws<ArgumentException>(() => 
             new User("test@mail.com", "+79180576819", firstName, "Иванов", "hash"));
     }
     
@@ -77,11 +78,11 @@ public class UserTests
     [InlineData(null)]
     [InlineData("")]
     [InlineData("text with whitespaces\r\n")]
-    public void When_CreateUser_WithInvalidLastName_ThrowsArgumentException(string lastName)
+    public void WhenCreateUser_WithInvalidLastName_ThrowsArgumentException(string lastName)
     {
         // Act
         // Assert
-        Assert.Throws<ArgumentException>(() => 
+        Throws<ArgumentException>(() => 
             new User("test@mail.com", "+79180576819", "Иван", lastName, "hash"));
     }
 }
