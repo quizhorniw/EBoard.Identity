17ec2b7385ce756e82ac57550effa35b85ed667a
Ревотюк Даниил <danistollen@yahoo.com>
2025-10-11 04:07:29 +0300

    Update User entity EFCore configuration

--- SolarLab.EBoard.Identity.Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ SolarLab.EBoard.Identity.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -9,6 +9,7 @@ internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id);
 
         builder.Property(u => u.Email).IsRequired().HasMaxLength(255);
         builder.HasIndex(u => u.Email).IsUnique();
@@ -16,7 +17,7 @@ internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(u => u.FirstName).IsRequired().HasMaxLength(100);
         builder.Property(u => u.LastName).IsRequired().HasMaxLength(100);
         builder.Property(u => u.PasswordHash).IsRequired();
-        builder.Property(u => u.Role).IsRequired();
+        builder.Property(u => u.Role).IsRequired().HasDefaultValue("User");
 
         builder.ToTable(t => t.HasCheckConstraint("CK_User_Role", "role IN ('Admin','User')"));
     }
