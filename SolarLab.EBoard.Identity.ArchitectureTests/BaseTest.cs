016a32b37faf5f3ea41e711a059d60a2744fc1c5
Ревотюк Даниил <danistollen@yahoo.com>
2025-10-12 16:01:03 +0300

    Architecture tests

---
+++ SolarLab.EBoard.Identity.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using SolarLab.EBoard.Identity.Application.Abstractions.Authentication;
+using SolarLab.EBoard.Identity.Domain.Entities;
+using SolarLab.EBoard.Identity.Infrastructure.Persistence;
+
+namespace SolarLab.EBoard.Identity.ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(IUserContext).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(AppDbContext).Assembly;
+    protected static readonly Assembly WebApiAssembly = typeof(Program).Assembly;
+}
